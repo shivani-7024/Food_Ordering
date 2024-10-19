ad1667587741878ef7f7ffca5f70b461b27bebbd
ShivaniGupta <vanigupta2428@gmail.com>
2024-10-19 22:43:58 +0530

    Cart And Order.

--- src/main/java/shivani/food_odering/FoodOrderingApplication.java
+++ src/main/java/shivani/food_odering/FoodOrderingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodOderingApplication {
+public class FoodOrderingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FoodOderingApplication.class, args);
+        SpringApplication.run(FoodOrderingApplication.class, args);
     }
 
 }
